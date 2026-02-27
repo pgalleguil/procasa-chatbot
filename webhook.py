86c5adafa38a83b357754e034b59ae20015a64ec
Pablo Galleguillos <p.galleguil@gmail.com>
2026-02-26 21:32:27 -0300

    Ajustes

--- webhook.py
+++ webhook.py
@@ -1055,7 +1055,7 @@ async def process_pending_leads_loop():
                             )
                             if success: mark_notification_sent(p["_id"])
 
-                        # Throttling Anti-Spam: 30 segundos entre ejecutivos (Aumentado por precaución de Meta)
+                        # TThrottling Anti-Spam: 30 segundos entre ejecutivos (Aumentado por precaución de Meta)
                         logger.info(f"[BACKGROUND] Pausa anti-spam (30s) para siguiente destinatario...")
                         await asyncio.sleep(30)
                         

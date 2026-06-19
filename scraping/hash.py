6c1bd79bcd25e7a83592294d74265b195849dcff
Pablo Galleguillos <p.galleguil@gmail.com>
2026-06-19 11:40:04 -0400

    aplicar gitignore correctamente

--- scraping/hash.py
+++
@@ -1,3 +0,0 @@
-import hashlib
-url = "https://www.yapo.cl/bienes-raices-venta-de-propiedades-apartamentos/departamento-en-venta-de-2-dorm-en-santiago/32159226"
-print(hashlib.md5(url.encode()).hexdigest() + ".html")
